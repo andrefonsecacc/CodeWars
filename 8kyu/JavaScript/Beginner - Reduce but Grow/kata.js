c433c065fade0f804b8b74b3d89bb52b592131cf
André Fonseca <andre.j.fonseca@gmail.com>
2021-02-21 17:21:19 +0000

    JS more 2 solutions
    More 2 JS 8kyu solutions

---
+++ 8kyu/JavaScript/Beginner - Reduce but Grow/kata.js
@@ -0,0 +1,5 @@
+function grow(x){
+ var res=1;
+ x.forEach(element => res *= element );
+ return res;
+}
---
+++ 8kyu/JavaScript/Find the position!/kata.js
@@ -0,0 +1,6 @@
+function position(letter){
+//Write your own Code!
+  var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  
+  return "Position of alphabet: "+ (alphabet.indexOf(letter)+1);
+}
